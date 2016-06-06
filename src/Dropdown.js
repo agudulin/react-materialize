e953c780c982c39c96e392898c2cc5375fbf86ea
Alexander Odell <alextrastero@gmail.com>
2016-06-06 20:17:04 +0200

    Initialize dropdown when mounted

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import idgen from './idgen';
 
-class Dropdown extends React.Component {
+class Dropdown extends Component {
   constructor(props) {
     super(props);
     this.renderTrigger = this.renderTrigger.bind(this);
   }
 
-  render() {
-    let {trigger, children, ...props} = this.props;
-    let idx = 'dropdown_' + idgen();
-    return (
-      <span>
-        {this.renderTrigger(idx)}
-        <ul className='dropdown-content' id={idx}>
-          {children}
-        </ul>
-      </span>
-    )
+  componentDidMount() {
+    $(this.refs[this.idx]).dropdown();
   }
 
-  renderTrigger(idx) {
-    let trigger = this.props.trigger;
-    return React.cloneElement(trigger,
-      {
+  render() {
+    const { children, ...props } = this.props;
+    this.idx = 'dropdown_' + idgen();
+
+    return (
+      <span props>
+        { this.renderTrigger() }
+        <ul className='dropdown-content' id={ this.idx }>
+          { children }
+        </ul>
+      </span>
+    );
+  }
+
+  renderTrigger() {
+    return React.cloneElement(
+      this.props.trigger, {
+        ref: this.idx,
         className: 'dropdown-button',
         'data-beloworigin': true,
-        'data-activates': idx
+        'data-activates': this.idx
       }
     );
   }
@@ -36,15 +41,16 @@ Dropdown.propTypes = {
   /**
    * The button to trigger the dropdown
    */
-  trigger: React.PropTypes.node.isRequired,
+  trigger: PropTypes.node.isRequired,
+  children: PropTypes.node,
   /**
    * 	If true, the dropdown will show over the activator. Default: false
    */
-  overorigin: React.PropTypes.bool,
+  overorigin: PropTypes.bool
 };
 
 Dropdown.defaultProps = {
-  overorigin: false,
-}
+  overorigin: false
+};
 
 export default Dropdown;
